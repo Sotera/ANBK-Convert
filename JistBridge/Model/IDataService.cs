8730365f336cb7699be2ee3b3809399179f200b3
Justin Lueders <Justin.Lueders@soteradefense.com>
2013-08-13 10:48:39 -0500

    Initial checkin of base Jist Bridge project.

---
+++ JistBridge/Model/IDataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JistBridge.Model
+{
+    public interface IDataService
+    {
+        void GetData(Action<DataItem, Exception> callback);
+    }
+}
