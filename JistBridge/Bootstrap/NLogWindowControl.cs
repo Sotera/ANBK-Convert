8750e57fa481774636bdcfea0c912432f680a214
John Reeme <john.reeme@soteradefense.com>
2013-08-23 16:20:45 -0500

    Fix build

---
+++ JistBridge/Bootstrap/NLogWindowControl.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows;
+using JistBridge.Interfaces;
+using JistBridge.Messages;
+using NLog;
+using NLog.Config;
+
+namespace JistBridge.Bootstrap {
+	[Export(typeof (IBootstrapTask))]
+	internal class NLogWindowControl : IBootstrapTask {
+		internal NLogWindowControl() {
+			ShowNLogWindowMessage.Register(this, msg => {
+				var asyncWrapper = ShowNLogWindowMessage.AsyncTargetWrapper;
+				var rtb = ShowNLogWindowMessage.RichTextBox;
+				rtb.Visibility = Visibility.Visible;
+
+				SimpleConfigurator.ConfigureForTargetLogging(asyncWrapper, LogLevel.Trace);
+				new AddRemoveDocumentViewMessage(null, null, cbMsg => { }) {
+					Operation = Operation.Add,
+					ReportView = rtb,
+					TabText = "NLog Output"
+				}.Send();
+			});
+		}
+	}
+}
