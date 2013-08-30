16581c868ec31cc6bbdcdee0a0b6e253591ffecf
John Reeme <john.reeme@soteradefense.com>
2013-08-30 15:28:17 -0500

    Forgot a couple.

---
+++ JistBridge/Messages/ANBK/AddIconToLocalChartMessage.cs
@@ -0,0 +1,7 @@
+﻿namespace JistBridge.Messages.ANBK {
+	public class AddIconToLocalChartMessage : BaseMessage<AddIconToLocalChartMessage> {
+		public string Label { get; set; }
+		public AddIconToLocalChartMessage(object sender, object target)
+			: base(sender, target) {}
+	}
+}
---
+++ JistBridge/Messages/ANBK/SendChainInfoToANBKMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace JistBridge.Messages.ANBK {
+	public class SendChainInfoToANBKMessage : BaseMessage<SendChainInfoToANBKMessage> {
+		public enum ActionType {
+			CreateNewChain,
+			ModifyLeftNodeText,
+			ModifyLinkText,
+			ModifyRightNodeText
+		};
+		public ActionType Action { get; set; }
+		public string Text { get; set; }
+		public int LeftNodeId;
+		public int LinkId;
+		public int RightNodeId;
+		public SendChainInfoToANBKMessage(object sender, object target, Action<SendChainInfoToANBKMessage> cb)
+			: base(sender, target, cb) {}
+	}
+}
